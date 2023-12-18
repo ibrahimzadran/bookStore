3a322f2498765a8c5d2340851c5177a89e351634
ibrahim Zadran <i.b.zadran@gmail.com>
2023-12-18 12:49:19 -0500

    added book dao interface

---
+++ src/main/java/BookDao.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public interface BookDao {
+
+    void addBook(Book book);
+    void updateBook(Book book);
+    void deleteBook(int bookId);
+
+    List<Book> getAllBooks();
+
+
+}
